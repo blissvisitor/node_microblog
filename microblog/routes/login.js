c48eb9ffa6454d504e4f49a2aac57e4aa11bb1fb
dqy <192949421@qq.com>
2016-12-13 12:56:26 +0800

    add project

---
+++ microblog/routes/login.js
@@ -0,0 +1,15 @@
+var express=require("express");
+var router=express.Router();
+router.get('/',function (req,res,next) {
+  res.render('login',{title:'登录',layout:'layout'});
+});
+router.post('/:username,:password',function (req,res,next) {
+  if(req.params.username){
+    res.render('login',{title:'登录',layout:'layout'});
+  }else{
+    next();
+
+  }
+});
+
+module.exports=router;
